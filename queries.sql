5b80c382f7928171853fdeddaa029b20da16c4be
Gonzalo Martone <gmartone@itba.edu.ar>
2023-09-04 15:01:06 -0300

    feat: query 2

--- queries.sql
+++ queries.sql
@@ -3,3 +3,12 @@
 SELECT nro_telefono, t.nro_cliente
 FROM e01_cliente JOIN e01_telefono t on e01_cliente.nro_cliente = t.nro_cliente
 WHERE nombre = 'Wanda' AND apellido = 'Baker';
+
+-- 2
+-- Seleccionar todos los clientes que tengan registrada al menos una factura.
+SELECT *
+FROM e01_cliente
+WHERE nro_cliente IN (
+    SELECT nro_cliente
+    FROM e01_factura
+);
