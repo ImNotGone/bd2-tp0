536a834ba53bf43c4cc0552563868c7509feb03e
Gonzalo Martone <gmartone@itba.edu.ar>
2023-09-05 11:43:31 -0300

    feat: view 2

--- views.sql
+++ views.sql
@@ -1,4 +1,16 @@
 -- 1
 -- Se debe realizar una vista que devuelva las facturas ordenadas por fecha.
 CREATE VIEW facturas_ordenadas AS
-SELECT * FROM e01_factura ORDER BY fecha;
+SELECT *
+FROM e01_factura
+ORDER BY fecha;
+
+-- 2
+-- Se necesita una vista que devuelva todos los productos que aun no han sido facturados.
+CREATE VIEW productos_no_facturados AS
+SELECT *
+FROM e01_producto
+WHERE codigo_producto NOT IN (
+    SELECT codigo_producto
+    FROM e01_detalle_factura
+);
