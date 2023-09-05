c732d9c168ba7532a9f50181104e85cf459d4650
Gonzalo Martone <gmartone@itba.edu.ar>
2023-09-05 19:16:58 -0300

    refactor: cleanup

--- api/index.js
+++ api/index.js
@@ -1,25 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const db = require('./queries')
 const port = 3000
 
 app.use(bodyParser.json())
-
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 )
 
+// Home
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
-
-app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
-
-const db = require('./queries')
 
 // 1
 // Dar de alta a nuevos clientes, baja y modificacion de los ya existentes
@@ -32,3 +27,7 @@ app.delete('/clients/:id', db.deleteClient)
 // Tenga en cuenta que el precio de un producto es sin IVA.
 api.post('/products', db.updateProduct)
 api.put('/products/:id', db.createProduct)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
