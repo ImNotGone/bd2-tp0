593cb4fd1593a17ee0c5a6596186d71cdcdca2e3
Gonzalo Martone <gmartone@itba.edu.ar>
2023-09-05 19:08:50 -0300

    chore: add calls to queries.js methods

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const app = express()
 const port = 3000
 
 app.use(bodyParser.json())
+
 app.use(
   bodyParser.urlencoded({
     extended: true,
@@ -17,3 +18,17 @@ app.get('/', (request, response) => {
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
+
+const db = require('./queries')
+
+// 1
+// Dar de alta a nuevos clientes, baja y modificacion de los ya existentes
+app.put('/clients', db.createClient)
+app.post('/clients/:id', db.updateClient)
+app.delete('/clients/:id', db.deleteClient)
+
+// 2
+// Dar de alta nuevos productos y modificacion de los ya existentes.
+// Tenga en cuenta que el precio de un producto es sin IVA.
+api.put('/products', db.createProduct)
+api.post('/products/:id', db.updateProduct)
