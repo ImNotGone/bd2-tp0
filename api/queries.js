f2b5a78ed1c3bf516fc66495a84c07a5d890cd02
Santiago Ballerini <sballerini@itba.edu.ar>
2023-10-10 17:00:19 -0300

    fix: fix server crashing on error

--- api/queries.js
+++ api/queries.js
@@ -1,73 +1,103 @@
-const { Pool } = require('pg');
-const fs = require('fs');
+const { Pool } = require("pg");
+const fs = require("fs");
 
 // Read the database configuration from db-config.json
-const dbConfig = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
+const dbConfig = JSON.parse(fs.readFileSync("credentials.json", "utf8"));
 
 // Create a PostgreSQL connection pool using the imported credentials
 const pool = new Pool(dbConfig);
 
 const createClient = (request, response) => {
-    const { nro_cliente, nombre, apellido, direccion, activo } = request.body
+    const { nro_cliente, nombre, apellido, direccion, activo } = request.body;
 
-    pool.query('INSERT INTO e01_cliente (nro_cliente, nombre, apellido, direccion, activo) VALUES ($1, $2, $3, $4, $5) RETURNING *', [nro_cliente, nombre, apellido, direccion, activo], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(201).send(`Client added with ID: ${nro_cliente}`)
-    })
-}
+    pool.query(
+        "INSERT INTO e01_cliente (nro_cliente, nombre, apellido, direccion, activo) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+        [nro_cliente, nombre, apellido, direccion, activo],
+        (error, results) => {
+            if (error) {
+                response.status(400).send("Error: " + error.detail);
+            } else {
+                response
+                    .status(201)
+                    .send(`Client added with ID: ${nro_cliente}`);
+            }
+        },
+    );
+};
 
 const updateClient = (request, response) => {
-    const id = parseInt(request.params.id)
-    const { nombre, apellido, direccion, activo } = request.body
+    const id = parseInt(request.params.id);
+    const { nombre, apellido, direccion, activo } = request.body;
 
-    pool.query('UPDATE e01_cliente SET nombre=$2, apellido=$3, direccion=$4, activo=$5 WHERE nro_cliente=$1 RETURNING *', [id, nombre, apellido, direccion, activo], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).send(`Client modified with ID: ${id}`)
-    })
-}
+    pool.query(
+        "UPDATE e01_cliente SET nombre=$2, apellido=$3, direccion=$4, activo=$5 WHERE nro_cliente=$1 RETURNING *",
+        [id, nombre, apellido, direccion, activo],
+        (error, results) => {
+            if (error) {
+                response.status(400).json({ error });
+            } else {
+                response.status(200).send(`Client modified with ID: ${id}`);
+            }
+        },
+    );
+};
 
 const deleteClient = (request, response) => {
-    const id = parseInt(request.params.id)
+    const id = parseInt(request.params.id);
 
-    pool.query('DELETE FROM e01_cliente WHERE nro_cliente = $1', [id], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).send(`Client deleted with ID: ${id}`)
-    })
-}
+    pool.query(
+        "DELETE FROM e01_cliente WHERE nro_cliente = $1",
+        [id],
+        (error, results) => {
+            if (error) {
+                response.status(400).json({ error });
+            } else {
+                response.status(200).send(`Client deleted with ID: ${id}`);
+            }
+        },
+    );
+};
 
 const createProduct = (request, response) => {
-    const { codigo_producto, marca, nombre, descripcion, precio, stock } = request.body
+    const { codigo_producto, marca, nombre, descripcion, precio, stock } =
+        request.body;
 
-    pool.query('INSERT INTO e01_producto (codigo_producto, marca, nombre, descripcion, precio, stock) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [codigo_producto, marca, nombre, descripcion, precio, stock], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(201).send(`Product added with ID: ${codigo_producto}`)
-    })
-}
+    pool.query(
+        "INSERT INTO e01_producto (codigo_producto, marca, nombre, descripcion, precio, stock) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+        [codigo_producto, marca, nombre, descripcion, precio, stock],
+        (error, results) => {
+            if (error) {
+                response.status(400).json({ error });
+            } else {
+                response
+                    .status(201)
+                    .send(`Product added with ID: ${codigo_producto}`);
+            }
+        },
+    );
+};
 
 const updateProduct = (request, response) => {
-    const id = parseInt(request.params.id)
-    const { marca, nombre, descripcion, precio, stock } = request.body
+    const id = parseInt(request.params.id);
+    const { marca, nombre, descripcion, precio, stock } = request.body;
 
-    pool.query('UPDATE e01_producto SET marca=$2, nombre=$3, descripcion=$4, precio=$5, stock=$6 WHERE codigo_producto=$1 RETURNING *', [id, marca, nombre, descripcion, precio, stock], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).send(`Product modified with ID: ${id}`)
-    })
-}
+    pool.query(
+        "UPDATE e01_producto SET marca=$2, nombre=$3, descripcion=$4, precio=$5, stock=$6 WHERE codigo_producto=$1 RETURNING *",
+        [id, marca, nombre, descripcion, precio, stock],
+        (error, results) => {
+            if (error) {
+                response.status(400).json({ error });
+            } else {
+                response.status(200).send(`Product modified with ID: ${id}`);
+            }
+        },
+    );
+};
 
 module.exports = {
     createClient,
     updateClient,
     deleteClient,
     createProduct,
-    updateProduct
-}
+    updateProduct,
+};
